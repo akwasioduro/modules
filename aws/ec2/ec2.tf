bffef2c8e3b713d5a1bb937d0e996ad5ae54de8a
akwasioduro <akwasioduro014@gmail.com.com>
2025-08-24 10:57:18 +0000

    change

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "practice" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = local.instance_type
   subnet_id = module.vpc.private_subnet_id
-  depends_on = [aws_subnet.private]
 
   tags = {
     Name = local.instance_name
