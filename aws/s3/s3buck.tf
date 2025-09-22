250b8f0ecf559bcc8df701350286740f2de0d2f0
akwasioduro <akwasioduro014@gmail.com.com>
2025-09-22 20:22:19 +0000

    final

--- aws/s3/s3buck.tf
+++ aws/s3/s3buck.tf
@@ -16,19 +16,4 @@ resource "aws_s3_bucket_public_access_block" "terrabuck" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "terraform-state-lock"
-  }
-}
-
 
